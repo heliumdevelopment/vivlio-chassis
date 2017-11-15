4309dab692b5a1d35ffed0568f376644524cdd1a
Nick Mealey <nick@heliumdev.com>
2017-11-14 19:44:34 -0800

    Fixed rackup not starting

--- config/init.rb
+++ config/init.rb
@@ -10,7 +10,7 @@ require 'find'
 ENV = Sinatra::Base.environment.to_s
 
 #load initializers and lib
-%w{config/initializers lib}.each do |load_path|
+%w{config/initializers}.each do |load_path|
   Find.find(load_path) do |f|
     require f unless f.match(/\/\..+$/) || File.directory?(f)
   end
@@ -19,4 +19,3 @@ end
 # Protection against several web-based attacks - needs configuring
 # require 'rack/protection'
 # use Rack::Protection
-
